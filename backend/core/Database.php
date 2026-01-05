5f9c4654c0551d65e8c3f1eac3aee6cc1de27290
PrayaaDikk <fadilprayadika@gmail.com>
2026-01-05 14:50:55 +0800

    add: config.xml and database configuration

---
+++ backend/core/Database.php
@@ -0,0 +1,20 @@
+<?php
+class Database {
+    private $pdo;
+
+    public function connect() {
+        if ($this->pdo == null) {
+            $config = simplexml_load_file(__DIR__ . "/../backend/config/config.xml");
+
+            $host = $config->database->host;
+            $db = $config->database->name;
+            $user = $config->database->user;
+            $pass = $config->database->password;
+
+            $this->pdo = new PDO(
+                "mysqli:host=$host;dbname=$db;charset=utf8",$user,$pass
+            );
+        }
+        return $this->pdo;
+    }
+}
